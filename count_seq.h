af26603e66322e08dfba62dfcd62f7935e3e62e4
Poonam <Sadhana@Sadhanas-MacBook-Air.local>
2017-02-04 10:54:11 -0500

    fix: fix path and add doc

--- count_seq.h
+++ count_seq.h
@@ -1,6 +1,7 @@
 #!/bin/bash
 #grep- searches for a pattern occuring in a file
 #adding a c means that the function will count the number of sequences
+#USAGE: bash count_seq_h
 grep -c ">"~/Desktop/data-shell/Desktop/data/genome.fa
 
 
